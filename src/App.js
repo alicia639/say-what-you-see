274de4196992fc642699e84c0c39d469d492fe66
alicia639 <alicia.emberso.bremer@gmail.com>
2020-07-22 16:19:23 -0700

    display imageS

--- src/App.js
+++ src/App.js
@@ -21,7 +21,36 @@ class App extends Component {
 
      return (
        <div className="App">
-         
+         <ImageSequence
+            img1 = {require('./Images/ahh.jpeg')} alt1 = "ahh"
+            img2 = {require('./Images/albums.jpeg')} alt2 = "albums"
+            img3 = {require('./Images/ant.jpeg')} alt3 = "ant"
+            img4 = {require('./Images/back.jpeg')} alt4 = "back"
+            img5 = {require('./Images/bruce_lee.jpeg')} alt5 = "bruce_lee"
+            img6 = {require('./Images/chew.jpeg')} alt6 = "chew"
+            img7 = {require('./Images/con.png')} alt7 = "con"
+            img8 = {require('./Images/door.jpeg')} alt8 = "door"
+            img9 = {require('./Images/dumbell.jpeg')} alt9 = "dumbell"
+            img10 = {require('./Images/ear.jpeg')} alt10 = "ear"
+            img11 = {require('./Images/eh.png')} alt11 = "eh"
+            img12 = {require('./Images/ella.jpeg')} alt12 = "ella"
+            img13 = {require('./Images/f.jpeg')} alt13 = "f"
+            img14 = {require('./Images/foot.jpeg')} alt14 = "foot"
+            img15 = {require('./Images/gaze.jpeg')} alt15 = "gaze"
+            img16 = {require('./Images/L.png')} alt16 = "L"
+            img17 = {require('./Images/lid.jpeg')} alt17 = "lid"
+            img18 = {require('./Images/moon.jpeg')} alt18 = "moon"
+            img19 = {require('./Images/ogre.png')} alt19 = "ogre"
+            img20 = {require('./Images/opal.jpeg')} alt20 = "opal"
+            img21 = {require('./Images/princes.jpeg')} alt21 = "princes"
+            img22 = {require('./Images/santa.jpeg')} alt22 = "santa"
+            img23 = {require('./Images/shin.jpeg')} alt23 = "shin"
+            img24 = {require('./Images/sleigh.jpeg')} alt24 = "sleigh"
+            img25 = {require('./Images/star.png')} alt25 = "star"
+            img26 = {require('./Images/taffy.jpeg')} alt26 = "taffy"
+            img27 = {require('./Images/thick.jpeg')} alt27 = "thick"
+            img28 = {require('./Images/umber.jpeg')} alt28 = "umbder"
+            img29 = {require('./Images/wake.jpeg')} alt29 = "wake"/>
        </div>
     );
   }
--- src/Components/ImageSequence.js
+++ src/Components/ImageSequence.js
@@ -4,7 +4,35 @@ import './ImageSequence.css';
 const ImageSequence = (props) => {
   return (
     <div className = 'ImageSequence'>
-
+        <img src = {props.img1} alt = {props.alt1}/>
+        <img src = {props.img2} alt = {props.alt2}/>
+        <img src = {props.img3} alt = {props.alt3}/>
+        <img src = {props.img4} alt = {props.alt4}/>
+        <img src = {props.img5} alt = {props.alt5}/>
+        <img src = {props.img6} alt = {props.alt6}/>
+        <img src = {props.img7} alt = {props.alt7}/>
+        <img src = {props.img8} alt = {props.alt8}/>
+        <img src = {props.img9} alt = {props.alt9}/>
+        <img src = {props.img10} alt = {props.alt10}/>
+        <img src = {props.img11} alt = {props.alt11}/>
+        <img src = {props.img12} alt = {props.alt12}/>
+        <img src = {props.img13} alt = {props.alt13}/>
+        <img src = {props.img14} alt = {props.alt14}/>
+        <img src = {props.img15} alt = {props.alt15}/>
+        <img src = {props.img16} alt = {props.alt16}/>
+        <img src = {props.img17} alt = {props.alt17}/>
+        <img src = {props.img18} alt = {props.alt18}/>
+        <img src = {props.img19} alt = {props.alt19}/>
+        <img src = {props.img20} alt = {props.alt20}/>
+        <img src = {props.img21} alt = {props.alt21}/>
+        <img src = {props.img22} alt = {props.alt22}/>
+        <img src = {props.img23} alt = {props.alt23}/>
+        <img src = {props.img24} alt = {props.alt24}/>
+        <img src = {props.img25} alt = {props.alt25}/>
+        <img src = {props.img26} alt = {props.alt26}/>
+        <img src = {props.img27} alt = {props.alt27}/>
+        <img src = {props.img28} alt = {props.alt28}/>
+        <img src = {props.img29} alt = {props.alt29}/>
     </div>
   );
 };
